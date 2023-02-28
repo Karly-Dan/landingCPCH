5a3a13c9ced816f5fdeb27be2bd90bf0e72788ab
Karly-Dan <daniela.kar99@gmail.com>
2023-02-27 22:18:11 -0600

    se modifican estilos en el input

--- components/input.vue
+++ components/input.vue
@@ -1,25 +1,31 @@
 <template>
-  <label for="">
-    <input type="text">
-  </label>
+    <input type="text"  :placeholder="placeholderInput">
 </template>
 
 <script>
 export default {
-
+  props: {
+    placeholderInput: {
+      type: String,
+      default: '',
+    },
+  },
 }
 </script>
 
 <style>
-label {
-  display: inline-grid;
-}
 input {
   border-radius: 23.5px;
-border: transparent;
-background: #f7f7f7;
-height: 47px;
-padding: 0 24px;
-color: gray;
+  border: transparent;
+  background: #f7f7f7;
+  height: 47px;
+  padding: 0 24px;
+  color: gray;
+  margin-top: 5px;
+  outline: none !important;
+  display: inline-grid;
+  margin-bottom: 10px;
+  margin-left: 11px;
+  color: #686464;
 }
 </style>
