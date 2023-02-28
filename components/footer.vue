af0fedf5b82ff4b6d7afa4408774d3c3ec0c766b
Karly-Dan <daniela.kar99@gmail.com>
2023-02-27 22:16:17 -0600

    se modifican estilos en colores y medidas  en footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,14 @@
 <template>
-<div>
+<div style="display: flex;">
     <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15032.756597558786!2d-99.2887873!3d19.6192096!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d219605c522755%3A0x62eb508444319f8d!2sConde%20Z%C3%A1rate%20%26%20Asociados%20S.C.!5e0!3m2!1ses!2smx!4v1677542298866!5m2!1ses!2smx" width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15032.756597558786!2d-99.2887873!3d19.6192096!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d219605c522755%3A0x62eb508444319f8d!2sConde%20Z%C3%A1rate%20%26%20Asociados%20S.C.!5e0!3m2!1ses!2smx!4v1677542298866!5m2!1ses!2smx" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-    <div>
-        <img src="../assets/llamada.png" alt="">
-        <p>5512543594</p>
-        <p>Revisa nuestro aviso de privacidad aqui</p>
+    <h1 style="width: 3px; background-color: #1187ce; margin-left: 14px;"></h1>
+    <div style="padding: 15px;">
+        <h1 style="font-size: 25px; color: #1187ce;">Contactanos</h1>
+        <img src="../assets/llamada.png" alt="" style="position: absolute; margin-top: -3px;">
+        <p style="position: relative; margin-left: 30px;">5512543594</p>
+        <p style="cursor: pointer;">Revisa nuestro aviso de privacidad aqui</p>
     </div>
 </div>
 </template>
