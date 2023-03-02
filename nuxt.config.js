c59927fb70f9aa69c754e727c31f5266901734fd
Karly-Dan <daniela.kar99@gmail.com>
2023-03-02 17:26:22 -0600

    modificscion del titulo de la pagina

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'landingCPCH',
+    title: 'Centro de Profesionalizacón de Capital Humano',
     htmlAttrs: {
       lang: 'en'
     },
