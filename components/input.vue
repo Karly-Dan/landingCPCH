56eb4ffacfbff57a9189684bdfe9d0190b1e2d25
Karly-Dan <daniela.kar99@gmail.com>
2023-03-14 02:16:59 -0600

    se modifica las medidas del input y se agrega medidas responivas

--- components/input.vue
+++ components/input.vue
@@ -1,5 +1,8 @@
 <template>
-    <input type="text"  :placeholder="placeholderInput">
+  <div class="contenedor-input">
+    <label class="label" for="">{{ titulo }}</label>
+    <input type="text"  :placeholder="placeholderInput" class="input-base">
+  </div>
 </template>
 
 <script>
@@ -9,23 +12,46 @@ export default {
       type: String,
       default: '',
     },
+    titulo: {
+      type: String,
+      default: '',
+    },
   },
 }
 </script>
 
 <style>
-input {
-  border-radius: 23.5px;
+.input-base {
+  cursor: pointer;
+  outline: none;
+  border-radius: 15px;
   border: transparent;
   background: #f7f7f7;
+  padding-left: 11px;
+  outline: none;
   height: 47px;
-  padding: 0 24px;
-  color: gray;
-  margin-top: 5px;
-  outline: none !important;
-  display: inline-grid;
-  margin-bottom: 10px;
   color: #686464;
-  max-width: 80%;
+  margin-top: 5px;
 }
+.contenedor-input {
+  margin-bottom: 10px;
+}
+
+
+
+@media (max-width: 776px) {
+  .input-base {
+    display: block;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .label {
+    margin-left: 12px;
+  }
+}
+
 </style>
+
+
+
