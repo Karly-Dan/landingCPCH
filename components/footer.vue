497e533b227c19957591d752cec9ada14c9e5945
Karly-Dan <daniela.kar99@gmail.com>
2023-03-14 02:15:44 -0600

    se agregan iconos y colores

--- components/footer.vue
+++ components/footer.vue
@@ -1,16 +1,20 @@
 <template>
-<div style="display: flex;">
-    <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3758.190995580349!2d-99.2910161857256!3d19.619130639774866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d2191273ac7405%3A0x9038e4b1068b2df1!2sCentro%20de%20Profesionalizaci%C3%B3n%20de%20Capital%20Humano!5e0!3m2!1ses-419!2smx!4v1677868003048!5m2!1ses-419!2smx" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <div class="contenedor-footer">
+    <div class="contenido-direccion">
+        <p class="direccion">Av. Camino a la presa #6, San Isidro la Paz, Nicolás Romero, Edo. Méx. C.P. 54477</p>
+        <p class="numero">55-1254-5494</p>
+        <p class="privacidad">Avisos de Privacidad</p>
     </div>
-    <h1 style="width: 3px; background-color: #1187ce; margin-left: 8px;"></h1>
-    <div style="padding: 4px;">
-        <h1 style="font-size: 25px; color: #1187ce;">Contactanos</h1>
-        <img src="../assets/llamada.png" alt="" style="position: absolute; margin-top: -3px;">
-        <p style="position: relative; margin-left: 30px;">5512543594</p>
-        <p style="cursor: pointer;">Revisa nuestro aviso de privacidad aqui</p>
+    <div class="redes">
+        <img src="../assets/whatsapp.png" alt="whats">
+        <img src="../assets/facebook(1).png" alt="facebook">
+        <img src="../assets/twitter.png" alt="twiter">
+        <img src="../assets/instagram.png" alt="">
+        <img src="../assets/google-mas.png" alt="google">
+        <img src="../assets/linkedin.png" alt="linked">
+        <img src="../assets/youtube(1).png" alt="youtube">
     </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -20,5 +24,29 @@ export default {
 </script>
 
 <style>
+.contenedor-footer {
+    background-color: #021373;
+    color: white;
+    text-align: center;
+}
+.redes {
+  filter: invert(100%) sepia(6%) saturate(7433%) hue-rotate(241deg) brightness(114%) contrast(91%);
+}
 
+
+@media (min-width: 1024px) {
+  .contenido-direccion {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .direccion, .privacidad {
+    border-right: 2px solid white ;
+    border-left: 2px solid white;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .redes {
+    padding-bottom: 10px;
+  }
+}
 </style>
